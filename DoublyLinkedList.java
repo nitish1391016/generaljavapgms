e653feb7550680da59388db943f6e4a49fcbcc79
nitishkumar139 <80934407+nitishkumar139@users.noreply.github.com>
2022-03-15 14:31:23 +0530

    Add files via upload

---
+++ DoublyLinkedList.java
@@ -0,0 +1,218 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+public class DoublyLinkedList {
+	private Node start,ptr,preptr;
+	
+	public Node getStart() {
+		return start;
+	}
+	public void insert(int ele) {
+		if(start==null) {		//first time 
+			start=new Node(null,ele,null);
+		}
+		else {
+			ptr=start;			//other
+			while(ptr.getNext()!=null)
+				ptr=ptr.getNext();
+			ptr.setNext(new Node(ptr,ele,null));
+		}
+	}
+	public void insert_begin(int ele)	{
+		if(start==null) {
+			start=new Node(null,ele,null);
+		}
+		else	{
+			start.setPre(new Node(null,ele,start));
+			start=start.getPre();
+		}
+	}
+	
+	public void insert_end(int ele)	{
+		if(start==null) {
+			start=new Node(null,ele,null);//if list is empty
+		}
+		else {
+			ptr=start;//moving to last node
+			while(ptr.getNext()!=null)
+				ptr=ptr.getNext();
+			ptr.setNext(new Node(ptr,ele,null));
+		}
+	}
+	public void insert_par(int par,int ele) throws Exception	{
+		if(start==null)	{
+			throw new Exception("list is empty!!!!");
+		}
+		if(start.getVal()==par)	{//if par is at first
+			insert_begin(ele);
+			return;
+		}
+		preptr=ptr=start;//if element is to be inserted other than first
+		while(ptr.getVal()!=par && ptr.getNext()!=null) {
+			preptr=ptr;
+			ptr=ptr.getNext();
+		}
+		if(ptr.getVal()!=par)
+			throw new Exception("element  not found!!!");
+		preptr.setNext(new Node(preptr,ele,ptr));
+		ptr.setPre(preptr.getNext());
+	}
+	
+	public void delete_begin() throws Exception	{
+		if(start==null) {
+			throw new Exception("list is empty!!!");
+		}
+		else
+			start=start.getNext();
+			start.setPre(null);
+	}
+	public void delete_end() throws Exception	{
+		if(start==null) {
+			throw new Exception("list is empty!!!");
+		}
+		else {
+			preptr=ptr=start;
+			while(ptr.getNext()!=null){//moving last node
+				preptr=ptr;
+				ptr=ptr.getNext();
+			}
+			preptr.setNext(null);
+		}
+	}
+	public void display() throws Exception {
+		if(start==null)
+			throw new Exception("list is empty");
+		ptr=start;
+		while(ptr!=null)	{
+			System.out.println(ptr.getVal());
+			ptr=ptr.getNext();
+		}
+	}
+	public int count() {
+		if(start==null)
+			return 0;
+		else {
+			ptr=start;
+			int count=0;
+			while(ptr!=null) {//same as display()
+				count++;
+				ptr=ptr.getNext();
+			}
+			return count;
+		}
+	}
+	public void delete_par(int par) throws Exception {
+		if(start==null) {
+			throw new Exception("list is empty!!!");
+		}
+		else {
+			if(start.getVal()==par)	{//if ele is first
+				delete_begin();
+				return;
+			}
+			preptr=ptr=start;
+			while(ptr.getVal()!=par && ptr.getNext()!=null) {//moving till element
+				preptr=ptr;						//else traverse till last
+				ptr=ptr.getNext();
+			}
+			if(ptr.getVal()!=par) {
+				throw new Exception("element not found!!!");
+			}
+			else {
+					preptr.setNext(ptr.getNext());
+			}
+		}
+	}
+	public boolean isEmpty()	{
+		if(start==null)
+			return true;
+		return false;
+	}
+	public static void main(String[] args) throws Exception {
+		try (Scanner sc = new Scanner(System.in)) {
+			DoublyLinkedList obj=new DoublyLinkedList();
+			System.out.println("1.insert\n2.insert_begin\n3.insert_end\n4.insert_par\n5.delete_begin\n6.delete_end\n7.delete_par\n8.display\n9.count\n10.isEmpty\n11.exit");
+			do {
+				int op=sc.nextInt();
+				switch(op)	{
+				case 1:obj.insert(sc.nextInt());
+					break;
+				case 2:obj.insert_begin(sc.nextInt());
+					break;
+				case 3:obj.insert_end(sc.nextInt());
+					break;
+				case 4:obj.insert_par(sc.nextInt(), sc.nextInt());
+					break;
+				case 5:obj.delete_begin();
+					break;
+				case 6:obj.delete_end();
+					break;
+				case 7:obj.delete_par(sc.nextInt());
+					break;
+				case 8:obj.display();
+					break;
+				case 9:System.out.println(obj.count());
+					break;
+				case 10:System.out.println(obj.isEmpty());
+					break;
+				case 11:System.exit(0);
+				}			
+			}while(true);
+		}
+
+	}
+
+}
+
+class Node {
+	private int val;
+	private Node next;
+	private Node pre;
+	
+	
+	public Node getPre() {
+		return pre;
+	}
+
+	public void setPre(Node pre) {
+		this.pre = pre;
+	}
+
+	public Node(int val)	{
+		this.val=val;
+	}
+	
+	public Node(Node next) {
+		this.next=next;
+	}
+	
+	public Node(Node pre,int val,Node next) {
+		this.pre=pre;
+		this.val=val;
+		this.next=next;
+	}
+	
+	public Node(int val,Node next)	{
+		this.val=val;
+		this.next=next;
+	}
+	
+	public int getVal() {
+		return val;
+	}
+	
+	public void setVal(int val) {
+		this.val = val;
+	}
+	public Node getNext() {
+		return next;
+	}
+	
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
+
+
+//
